f2de6a04a8298d140e632072044b7268325dfc55
geanamonte <geanamonte@gmail.com>
2024-10-16 21:04:14 -0400

    add many-to-many relationship between tag and item

--- resources/tag.py
+++ resources/tag.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
-from models import StoreModel, TagModel
-from schemas import StoreSchema,TagSchema
+from models import StoreModel, TagModel, ItemModel
+from schemas import StoreSchema,TagSchema, TagAndItemSchema
 from db import db
 
 blp = Blueprint("tags", __name__, description="Operations on tags")
+
 
 @blp.route("/store/<string:store_id>/tag")
 class TagInStore(MethodView):
@@ -28,10 +29,54 @@ class TagInStore(MethodView):
             abort(500, message=str(e))
         return tag
 
-@blp.route("/tag")
-class Tag(MethodView):
-    def get(self, tag_id):
-        raise NotImplementedError("Not implemented")
+@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+class LinkTagsToItem(MethodView):
+    @blp.response(201, TagSchema)
+    def post(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+        item.tags.append(tag)
 
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            abort(500, message=str(e))
+        return tag
+
+    @blp.response(200, TagAndItemSchema)
+    def delete(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+        item.tags.remove(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            abort(500, message=str(e))
+        return {"message": "Item removed from tag", "item": item, "tag": tag}
+
+
+@blp.route("/tag/<string:tag_id>")
+class Tag(MethodView):
+    @blp.response(200, TagSchema)
+    def get(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+        return tag
+
+    @blp.response(202, description="Deletes a tag if no item is tagged with it")
+    @blp.alt_response(404, description="Tag not found.") # Add additional responses
+    @blp.alt_response(400, description="Returned if the tag is assigned to one or more items. In this case, the tag is not deleted.")
     def delete(self, tag_id):
-        raise NotImplementedError("Not implemented")
+        tag = TagModel.query.get_or_404(tag_id)
+
+        try:
+            if tag.items:
+                abort(400, message="Could not delete tag. Make sure tag is not associated with any items, then try again.")
+            else:
+                db.session.delete(tag)
+                db.session.commit()
+        except SQLAlchemyError as e:
+            abort(500, message=str(e))
+        return {"message": "Tag deleted."}
