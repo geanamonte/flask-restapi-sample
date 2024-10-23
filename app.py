e2dfd9afd0a05ad11f3f847d5244c4c7063042c8
geanamonte <geanamonte@gmail.com>
2024-10-23 17:47:23 -0400

    add jwt authentication

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
+import secrets
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 from db import db
-import models
+
 
 
 def create_app(db_url=None):
@@ -19,10 +22,14 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
     db.init_app(app)
 
     api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = "257486928229583289992490971392008765501"
+    jwt = JWTManager(app)
 
     with app.app_context():
         db.create_all()
@@ -30,5 +37,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
