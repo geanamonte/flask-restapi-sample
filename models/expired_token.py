27cb2f6e5a2db2010aca78356e564e620b130735
geanamonte <geanamonte@gmail.com>
2024-10-24 18:11:50 -0400

    add database migration and description column to items

--- models/expired_token.py
+++ models/expired_token.py
@@ -4,4 +4,4 @@ from db import db
 class ExpiredTokenModel(db.Model):
     __tablename__ = "expired_token"
 
-    token = db.Column(db.String(256), unique=True, nullable=False, primary_key=True)
+    token = db.Column(db.String(255), unique=True, nullable=False, primary_key=True)
