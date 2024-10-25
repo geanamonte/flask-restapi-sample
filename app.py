8ab0d76b4194d4b1e5cc8cf2ef4ee61bad1423e9
geanamonte <geanamonte@gmail.com>
2024-10-25 13:18:47 -0400

    fix docker deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import secrets
+
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
@@ -25,7 +25,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    print(app.instance_path)
+
     db.init_app(app)
     migrate = Migrate(app, db)
 
